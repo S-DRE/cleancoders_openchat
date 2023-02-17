695e26429f3b8003d52841b5a74a72831992a312
S-DRE <sdreworks@gmail.com>
2023-02-17 16:48:06 +0100

    RED: Created new test, updated a couple of dependencies and created the User class

---
+++ src/main/java/org/openchat/domain/users/User.java
@@ -0,0 +1,48 @@
+package org.openchat.domain.users;
+
+import java.util.Objects;
+
+public class User {
+
+
+    private final String id;
+    private final String username;
+    private final String password;
+    private final String about;
+
+    public User(String Id, String username, String password, String about) {
+        id = Id;
+        this.username = username;
+        this.password = password;
+        this.about = about;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(username, user.username) && Objects.equals(password, user.password) && Objects.equals(about, user.about);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, password, about);
+    }
+}
--- src/test/java/org/openchat/UsersAPIShould.java
+++ src/test/java/org/openchat/UsersAPIShould.java
@@ -8,10 +8,16 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.openchat.api.UsersAPI;
 import org.openchat.domain.users.RegistrationData;
+import org.openchat.domain.users.User;
 import org.openchat.domain.users.UserService;
 import spark.Request;
 import spark.Response;
 
+
+import java.util.UUID;
+
+import static org.assertj.core.api.FactoryBasedNavigableListAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
@@ -21,7 +27,9 @@ public class UsersAPIShould {
     private static final String USERNAME = "Alice";
     private static final String PASSWORD = "lñ$as#di34asd2#";
     private static final String ABOUT = "About Alice";
+    private static final String USER_ID = UUID.randomUUID().toString();
     private static final RegistrationData REGISTRATION_DATA = new RegistrationData(USERNAME, PASSWORD, ABOUT);
+    private static final User USER = new User(USER_ID, USERNAME, PASSWORD, ABOUT);
     @Mock
     Request request;
     @Mock
@@ -43,6 +51,26 @@ public class UsersAPIShould {
         given(request.body()).willReturn(jsonContaining(REGISTRATION_DATA));
         usersAPI.createUser(request, response);
         verify(userService).createUser(REGISTRATION_DATA);
+    }
+
+    @Test
+    public void returnJSONRepresentingANewlyCreatedUser() {
+        given(request.body()).willReturn(jsonContaining(REGISTRATION_DATA));
+        given(userService.createUser(REGISTRATION_DATA)).willReturn(USER);
+
+        String result = usersAPI.createUser(request, response);
+
+        verify(response).status(201);
+        verify(response).type("application/json");
+        assertEquals(result, jsonContaining(USER));
+    }
+
+    private String jsonContaining(User user) {
+        return new JsonObject()
+                .add("id", user.getId())
+                .add("username", user.getUsername())
+                .add("about", user.getAbout())
+                .toString();
     }
 
     private String jsonContaining(RegistrationData registrationData) {
